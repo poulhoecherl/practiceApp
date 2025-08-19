f530715110b32dd5f4435963ac16b6fe55ae1aad
Paul Hoecherl <p.heck@live.com>
2025-08-19 14:31:29 -0700

    Add LiteDB integration and update service interfaces
    - Updated `Practice.Data.csproj` to copy `practice.db` to output.
    - Added new binary file `practice.db`.
    - Modified `Practice.Services.csproj` to include `Autofac` package.
    - Implemented `DataService` class in `DataService.cs` for LiteDB interactions.
    - Updated `IDataService` interface to define methods for data retrieval.

---
+++ Practice.Services/Services/DataService.cs
@@ -0,0 +1,57 @@
+﻿using LiteDB;
+using Practice.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Services.Services
+{
+    public class DataService : IDataService
+    {
+        private readonly string dbName = "practice.db";
+
+        private string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new ArgumentNullException("Assembly path is null");
+
+        internal LiteDatabase GetDatabase()
+        {
+
+            using var database = new LiteDatabase(dbName);
+            return database;
+        }
+
+        public Drill GetDrill(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Drill> GetDrills()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Song GetSession(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Session> GetSessions()
+        {
+            using var db = new LiteDatabase(dbName);
+
+        }
+
+        public Song GetSong(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Song> GetSongs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Practice.Services/Services/IDataService.cs
@@ -0,0 +1,19 @@
+﻿using Practice.Data.Models;
+
+namespace Practice.Services.Services
+{
+    public interface IDataService
+    {
+        List<Song> GetSongs();
+
+        Song GetSong(string name);
+
+        List<Session> GetSessions();
+        
+        Song GetSession(int id);
+
+        List<Drill> GetDrills();
+
+        Drill GetDrill(int id);
+    }
+}
